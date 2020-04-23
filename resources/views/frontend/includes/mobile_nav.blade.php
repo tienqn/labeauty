7c34fd42e2dfe0ec016fa9d07968021b0a6760a2
tienpham <tiens8acc@enjoyworks.co.kr>
2020-04-23 10:38:26 +0700

    commit

--- resources/views/frontend/includes/mobile_nav.blade.php
+++ resources/views/frontend/includes/mobile_nav.blade.php
@@ -1,28 +1,29 @@
 <!-- Main menu -->
 <header id="lb-mobile-header">
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		<a class="navbar-brand" href="#">
+		<a class="navbar-brand lb-logo" href="#">
 			<img src="logo2.png" alt="">
 		</a>
+		<p class="lb-slogan"><span class="lb-branch-name">LABEAUTY</span> CHUYÊN ĐIỀU TRỊ DA</p>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 			<span class="navbar-toggler-icon"></span>
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
-		<div class="collapse navbar-collapse">
+		<div class="collapse navbar-collapse lb-navbar-collapse">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item active">
-					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					<a class="nav-link" href="#">TRANG CHỦ <span class="sr-only">(current)</span></a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Home</a>
+					<a class="nav-link" href="#">DỊCH VỤ</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Home</a>
+					<a class="nav-link" href="#">KẾT QUẢ ĐIỀU TRỊ</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="#">Home</a>
+					<a class="nav-link" href="#">LIÊN HỆ</a>
 				</li>
 			</ul>
 		</div>
