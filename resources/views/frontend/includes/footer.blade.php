0504469898908abd843995b7af0cbe0935906471
tienpham <tiens8acc@enjoyworks.co.kr>
2020-04-27 11:30:52 +0700

    commit

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -20,6 +20,7 @@
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
 					<div class="beautypress-single-footer beautypress-single-footer-group">
+						<p class="work_time">GIỜ LÀM VIỆC</p>
 						<p>Thứ 2 - Thứ 6: 08:00 – 18:00</p>
 						<p>Thứ 7: 09:00 – 16:00</p>
 						<p>Chủ nhật: Đóng cửa</p>
@@ -27,6 +28,7 @@
 				</div>
 				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
 					<div class="beautypress-single-footer beautypress-single-footer-group">
+						<p class="work_time">LIÊN HỆ</p>
 						<p>La beauty - 08 Nguyễn Trường Tộ - Đà Nẵng</p>
 						<p>Email: {{ config('app.admin_email') }}</p>
 						<p>Điện thoại: {{ config('app.admin_phone') }}</p>
@@ -50,7 +52,7 @@
 				</div><!-- .beautypress-back-to-top-wraper END -->
 			</div>
 			<div class="beautypress-copyright-text">
-				<p>Bản quyền thuộc về &copy; Labeauty</p>
+				<p>Bản quyền thuộc về &copy; <span>Labeauty</span></p>
 			</div>
 		</div>
 	</div>
