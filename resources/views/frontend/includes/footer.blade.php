10d1cb2e23243ee7584106d2a1531c0a491e3f32
tienpham <tiens8acc@enjoyworks.co.kr>
2020-04-27 10:25:13 +0700

    commit

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,41 +1,41 @@
 <!-- Footer section -->
-<footer class="beautypress-footer-section">
+<footer class="beautypress-footer-section" id="lb-footer">
 	<div class="container">
 		<div class="beautypress-footer-content">
 			<div class="beautypress-footer-logo">
-				<a href="index.html">
+				<!-- <a href="index.html">
 					<img src="theme/img/footer_logo.png" alt="">
-				</a>
+				</a> -->
 			</div><!-- .beautypress-footer-logo END -->
 			<div class="beautypress-subscribe-wraper">
-				<h3>Subscribe For More Updates</h3>
+				<h3>TIN KHUYẾN MÃI</h3>
 				<form action="#" method="POST" class="mc-form">
 					<div class="beautypress-subscribe">
-						<input type="email" placeholder="Enter your email address....." id="mc-email">
-						<input type="submit" value="subscribe" class="beautypress-sub">
+						<input type="email" placeholder="Vui lòng nhập email" id="mc-email">
+						<input type="submit" value="Đăng kí" class="beautypress-sub">
 						<label for="mc-email"></label>
 					</div><!-- .beautypress-subscribe END -->
 				</form>
 			</div><!-- .beautypress-subscribe-wraper END -->
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
+					<div class="beautypress-single-footer beautypress-single-footer-group">
+						<p>Thứ 2 - Thứ 6: 08:00 – 18:00</p>
+						<p>Thứ 7: 09:00 – 16:00</p>
+						<p>Chủ nhật: Đóng cửa</p>
+					</div><!-- .beautypress-single-footer END -->
+				</div>
+				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
+					<div class="beautypress-single-footer beautypress-single-footer-group">
+						<p>La beauty - 08 Nguyễn Trường Tộ - Đà Nẵng</p>
+						<p>Email: {{ config('app.admin_email') }}</p>
+						<p>Điện thoại: {{ config('app.admin_phone') }}</p>
+					</div><!-- .beautypress-single-footer END -->
+				</div>
+				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
 					<div class="beautypress-single-footer">
 						<p>Then an unknown printer took a galley of type and scrambled it to make a type
 							specimen book. It has survived not only five.</p>
-					</div><!-- .beautypress-single-footer END -->
-				</div>
-				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
-					<div class="beautypress-single-footer beautypress-single-footer-group">
-						<p>Mon-Fri: 9 AM – 6 PM</p>
-						<p>Saturday: 9 AM – 4 PM</p>
-						<p>Sunday: Closed</p>
-					</div><!-- .beautypress-single-footer END -->
-				</div>
-				<div class="col-md-12 col-sm-12 col-lg-4 col-xl-4">
-					<div class="beautypress-single-footer beautypress-single-footer-group">
-						<p>176 W street name, New York, NY 10014</p>
-						<p>Email: info@yoursite.com</p>
-						<p>Telephone: +1(800)123-4566</p>
 					</div><!-- .beautypress-single-footer END -->
 				</div>
 			</div>
@@ -50,7 +50,7 @@
 				</div><!-- .beautypress-back-to-top-wraper END -->
 			</div>
 			<div class="beautypress-copyright-text">
-				<p><a href="https://xpeedstudio.com">XpeedStudio</a> &copy; 2018 All Rights Reserved.</p>
+				<p>Bản quyền thuộc về &copy; Labeauty</p>
 			</div>
 		</div>
 	</div>
