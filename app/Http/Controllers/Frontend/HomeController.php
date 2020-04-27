9ba35bb76df0f727f2df675e1af1a6ca7286a0b5
tienpham <tiens8acc@enjoyworks.co.kr>
2020-04-27 16:00:11 +0700

    commit

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-
+use Illuminate\Http\Request;
+use App\Repositories\Frontend\BookingRepository;
 /**
  * Class HomeController.
  */
---
+++ app/Repositories/Frontend/BookingRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repositories\Frontend;
+
+use App\Models\Booking\LbBooking;
+use Illuminate\Support\Facades\DB;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class BookingRepository.
+ */
+class BookingRepository extends BaseRepository
+{
+    /**
+     * BookingRepository constructor.
+     *
+     * @param  LbBooking  $model
+     */
+    public function __construct(LbBooking $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @throws \Exception
+     * @throws \Throwable
+     * @return \Illuminate\Database\Eloquent\Model|mixed
+     */
+    public function create(array $data)
+    {
+        return DB::transaction(function () use ($data) {
+            $service = $this->model::create([
+                'service_id' => $data['service_id'],
+                'booking_date' => $data['booking_date'],
+				'booking_time' => $data['booking_time'],
+				'name' => $data['name'],
+				'email' => $data['email'],
+				'phone_number' => $data['phone_number'],
+				'request' => $data['request'],
+                'is_read' => false
+            ]);
+
+            // Return the service object
+            return $service;
+        });
+    }
+}
