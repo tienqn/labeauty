c3046852c35f57bb100ad840055db6daa7d3a62b
tienpham <tiens8acc@enjoyworks.co.kr>
2020-04-28 13:01:17 +0700

    commit

--- public/backend/js/booking.js
+++
@@ -1,7 +0,0 @@
-
-$('.lb-table tbody tr').on('click', function() {
-	const url = this.dataset.href;
-	window.location.href = url;
-});
-
-$('.main ol.breadcrumb li:first-child').remove();
--- public/backend/js/labeauty.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-	$.getScript('../../backend/js/booking.js', function(){});
-});	
